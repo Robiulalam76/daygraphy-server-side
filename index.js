192c874c28ebd7497b8b31d2902e4cb0ee014fdb
robiulalam76 <robiulalam762400@gmail.com>
2022-11-09 02:59:00 +0600

    jwt system install

--- index.js
+++ index.js
@@ -13,10 +13,32 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yhjanrk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('dayGraphy').collection('services')
         const reviewCollection = client.db('dayGraphy').collection('reviews')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -35,6 +57,13 @@ async function run() {
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            let query = {}
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray()
             res.send(result)
         })
 
