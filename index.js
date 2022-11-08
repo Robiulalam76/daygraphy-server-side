d5ffb7f3b22fc88b91bc77a2f7983e2659c87070
robiulalam76 <robiulalam762400@gmail.com>
2022-11-08 20:49:33 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
-// const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middle were
 app.use(cors());
 app.use(express.json());
 require('dotenv').config()
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yhjanrk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('dayGraphy').collection('services')
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
+
 
 app.get('/', (req, res) => {
     res.send('daygraphy Is Running')
